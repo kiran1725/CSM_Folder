868c25a8f0f8559cb4e81d0897d088ab2a103d9a
kiran1725 <kiranbyala18@gmail.com>
2026-06-13 12:51:06 +0530

    Updated database configuration

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("https://csm-folder.onrender.com/")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
